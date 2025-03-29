97630ad1ca0270b21cd8b7eff6d9b8fe01227d3c
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-03-29 15:44:52 +0530

    Add routes index file

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
+const docsRoute = require('./docs.route');
 const config = require('../../config/config');
 
 const router = express.Router();
@@ -16,9 +17,23 @@ const defaultRoutes = [
   },
 ];
 
-// Register all routes
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
+];
+
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
 
 module.exports = router;
